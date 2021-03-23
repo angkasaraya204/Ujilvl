22e2898d0ba9c6977ef607734994d02667fb1eb4
Angkasa Raya <68907186+angkasaraya204@users.noreply.github.com>
2021-03-23 19:24:56 +0700

    Add files via upload

---
+++ ujilvl3/php/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host ="localhost";
+$user ="lvl3";
+$password ="1234";
+$database ="ujilvl3";
+$connect = mysqli_connect ($host,$user,$password,$database) or die("Gagal Menghubungkan"); //mengecek apakah sudah terkoneksi dengan database
+?>
