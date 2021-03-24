a1e4dc93cdb6057625dcd9098be483d6429b385a
Angkasa Raya <68907186+angkasaraya204@users.noreply.github.com>
2021-03-24 13:32:42 +0700

    Delete ujilvl2 directory

--- ujilvl2/php/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host ="localhost";
-$user ="starmart";
-$password ="";
-$database ="level2";
-$connect =mysqli_connect ($host,$user,$password,$database) or die("Gagal Menghubungkan"); //mengecek apakah sudah terkoneksi dengan database
-?>
